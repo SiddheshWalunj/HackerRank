e57c91a2b340791405f271f1858e8701bc6da00d
Siddhesh Walunj <snwalunj007@gmail.com>
2024-04-17 15:03:13 +0530

    This is the basic exmple of tuples in python from hackerrank

---
+++ Tuples.py
@@ -0,0 +1,34 @@
+# Task
+# Given an integer, n, and n space-separated integers as input, create a tuple, t, of those n integers.
+# Then compute and print the result of hash(t).
+#
+# Note: hash() is one of the functions in the __builtins__ module, so it need not be imported
+#
+# Input Format
+# The first line contains an integer, n, denoting the number of elements in the tuple.
+#
+# The second line contains n space-separated integers describing the elements in tuple t.
+#
+# Output Format
+# Print the result of hash(t).
+#
+# Sample
+# Input
+# 0
+# 2
+#
+# 1
+# 2
+#
+# Sample
+# Output
+# 0
+# 3713081631934410656
+#
+# (we have used pypy3 language to solve this problem)
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    integer_list = tuple(integer_list)
+    print(hash(integer_list))
